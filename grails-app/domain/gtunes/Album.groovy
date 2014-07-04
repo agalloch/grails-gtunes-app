4690fc296fe31e4d4e24cf6be01f416f403d6618
agalloch <the.dreadful.hours@gmail.com>
2014-07-04 18:55:06 +0300

    generated static scaffolding for Album

--- grails-app/domain/gtunes/Album.groovy
+++ grails-app/domain/gtunes/Album.groovy
@@ -2,6 +2,12 @@ package gtunes
 
 class Album {
     String title
+    Date yearReleased
 
     static hasMany = [songs: Song]
+
+    @Override
+    String toString() {
+        return title
+    }
 }
