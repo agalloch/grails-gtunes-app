c4cbaa5e6027fc395ea5099a6b1cf8297f5341e3
agalloch <the.dreadful.hours@gmail.com>
2014-07-04 13:55:26 +0300

    added StoreController

---
+++ test/unit/gtunes/StoreControllerTests.groovy
@@ -0,0 +1,14 @@
+package gtunes
+
+import grails.test.mixin.TestFor
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(StoreController)
+class StoreControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
