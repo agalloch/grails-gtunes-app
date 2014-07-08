5a534f418f31e7232fba2253babdb6d118522405
agalloch <the.dreadful.hours@gmail.com>
2014-07-08 10:04:15 +0300

    fixed failing unit test for Song

--- test/unit/gtunes/SongTests.groovy
+++ test/unit/gtunes/SongTests.groovy
@@ -1,9 +1,8 @@
 package gtunes
 
 
-
 import grails.test.mixin.*
-import org.junit.*
+import org.junit.Assert.*
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -11,7 +10,14 @@ import org.junit.*
 @TestFor(Song)
 class SongTests {
 
-    void testSomething() {
-       fail "Implement me"
+    void testMinimumDuration() {
+        mockForConstraintsTests(Song)
+
+        def song = new Song(title: 'Test Title',
+                            artist: 'Test Artist',
+                            duration: 0)
+
+        assertFalse('failed validation passed', song.validate())
+        assertEquals('duration is wrong', 'min', song.errors['duration'])
     }
 }
